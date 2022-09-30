ad2ef5d823f22abf995e0ad4c4b3201ac8cfbb29
weihang <weihang@xiaomi.com>
2022-09-30 18:19:56 +0800

    修改为本地主题

--- src/component/MenuLeft/Theme.js
+++ src/component/MenuLeft/Theme.js
@@ -7,6 +7,7 @@ import {replaceStyle} from "../../utils/helper";
 import TEMPLATE from "../../template/index";
 import "./Theme.css";
 import axios from "axios";
+import localThemeData from "../../json/localThemeList.json";
 
 @inject("content")
 @inject("navbar")
@@ -57,11 +58,12 @@ class Theme extends React.Component {
         remoteThemelist = response.data.data;
       } else {
         // 否则默认主题
-        response = await axios.get(THEME_API());
-        if (!response.data.success) {
-          throw new Error();
-        }
-        remoteThemelist = response.data.data.themeList;
+        // response = await axios.get(THEME_API());
+        // if (!response.data.success) {
+        //   throw new Error();
+        // }
+        // remoteThemelist = response.data.data.themeList;
+        remoteThemelist = localThemeData;
       }
 
       themeList = [
